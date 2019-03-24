7c61f7f7a3ef56a4f1448f109776685200de2396
Oliver Topliss <oliver.topliss1603@gmail.com>
2019-03-24 22:11:35 +0000

    created a toString method for the board and also written the constructor method. Part way through writing the initialise() method

--- Board.java
+++ Board.java
@@ -1,17 +1,41 @@
 public class Board
 {
+  //stores the 2d array of the board
+  private String[][] board;
+
   public Board()
   {
+    for (int row = 1; row <= 8; row++)
+    {
+      for (int column = 1; column <= 8; column++)
+      {
+        board[row - 1][column - 1] = ".";
+      }// for
+    }// for
 
-  }//Board Constructor
+  }// Board Constructor
 
-  private void initialise()
+  // method thast is used to intialisie the board to starting conditions
+  // takes an array of pieces whichg are to be added to the board
+  private void initialise(Piece[] arrayOfPieces)
   {
-
-  }//initialise Method
+  }// initialise Method
 
   private void clearPosition(String xCoordinate, int yCoordinate)
   {
 
-  }//clearPosition method
-}//Board Class
+  }// clearPosition method
+
+  @Override
+  public String toString()
+  {
+    for (int row = 1; row <= 8; row++)
+    {
+      for (int column = 1; column <= 8; column++)
+      {
+        System.out.print(board[row - 1][column - 1]);
+      }// for
+      System.out.println();
+    }// for
+  }// toString
+}// Board Class
