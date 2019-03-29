7b3ebdb8709114d6cce7ca7af9e85450b2e77f24
Oliver Topliss <oliver.topliss1603@gmail.com>
2019-03-29 23:25:46 +0000

    Implemented the move method for the Bishop
    The move method accepts 2 ints that represent the end coordinates. If the absolute between the current x and destination x and currenty and destination y are equal then the board is updated and so are the pieces coordinates

--- com/olivertopliss/pieces/Bishop.java
+++ com/olivertopliss/pieces/Bishop.java
@@ -1,16 +1,24 @@
 package com.olivertopliss.pieces;
+import static java.lang.Math.abs;
+
 public class Bishop extends Piece
 {
-
   public Bishop(String team)
   {
     super(team);
   }//Bishop Constructor
 
   @Override
-  public void move()
+  public void move(int xDestination, int yDestinatiion)
   {
-
+    //checks if the requested move is legal
+    if(abs(currentXCoordinate - xDestination) == abs(currentYCoordinate - yDestinatiion))
+    {
+      board[currentXCoordinate - 1][currentYCoordinate - 1] = null;
+      currentXCoordinate = xDestination;
+      currentYCoordinate = yDestinatiion;
+      board[xDestination - 1][yDestinatiion - 1] = this;
+    }
   }//move method
 
   @Override
