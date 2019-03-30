edbeef88743b29bfcd7ad9dd7ede3296734e924d
Oliver Topliss <oliver.topliss1603@gmail.com>
2019-03-30 23:44:50 +0000

    added implementation for the move method and changed constrcutor to take and provide parameters and arguments

--- com/olivertopliss/pieces/King.java
+++ com/olivertopliss/pieces/King.java
@@ -1,15 +1,30 @@
 package com.olivertopliss.pieces;
+
+import com.olivertopliss.game.Board;
+import static java.lang.Math.abs;
+
 public class King extends Piece
 {
 
-  public King(String team)
+  public King(String team, int startXCoordinate, int startYCoordinate)
   {
-    super(team);
+    super(team, startXCoordinate, startYCoordinate);
   }//King Constructor
 
   @Override
-  public void move()
+  public void move(int xDestination, int yDestination)
   {
+    //if both differences are 1 the diagonal move but if only one of them is 1 then its vertical or horizontal
+    if((abs(getCurrentXCoordinate() - xDestination) == 1 || (abs(getCurrentYCoordinate() - yDestination) == 1)))
+    {
+      //clears the bishops current position
+      Board.getBoard()[getCurrentXCoordinate() - 1][getCurrentXCoordinate() - 1] = null;
+      //updates the bishops coordinates
+      setCurrentXCoordinate(xDestination);
+      setCurrentYCoordinate(yDestination);
+      //moves the bishop to the new  location
+      Board.getBoard()[xDestination - 1][yDestination - 1] = this;
+    }// if
 
   }//move method
 
