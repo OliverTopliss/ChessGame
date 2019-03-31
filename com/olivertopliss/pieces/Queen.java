70796b2ef48bb4ffa7eac5c5062df6f61873a1f3
Oliver Topliss <oliver.topliss1603@gmail.com>
2019-03-31 22:55:17 +0100

    added implementation for the move method

--- com/olivertopliss/pieces/Queen.java
+++ com/olivertopliss/pieces/Queen.java
@@ -1,4 +1,8 @@
 package com.olivertopliss.pieces;
+
+import com.olivertopliss.game.Board;
+import static java.lang.Math.abs;
+
 public class Queen extends Piece
 {
 
@@ -9,9 +13,21 @@ public class Queen extends Piece
 
 
   @Override
-  public void move()
+  public void move(int xDestination, int yDestination)
   {
-
+    // allows for movement on a diagonal (same x and y diff) or vertical (xdiff = 0) and horizontal motion (ydiff = 0)
+    if (abs(getCurrentXCoordinate() - xDestination) == 0 && abs(getCurrentYCoordinate() - yDestination) > 0
+       || (abs(getCurrentXCoordinate() - xDestination) > 0 && abs(getCurrentYCoordinate() - yDestination) == 0)
+       || (abs(getCurrentXCoordinate() - xDestination) == abs(getCurrentYCoordinate() - yDestination)))
+    {
+      //clears the queen current position
+      Board.getBoard()[getCurrentXCoordinate() - 1][getCurrentXCoordinate() - 1] = null;
+      //updates the queen coordinates
+      setCurrentXCoordinate(xDestination);
+      setCurrentYCoordinate(yDestination);
+      //moves the queen to the new  location
+      Board.getBoard()[xDestination - 1][yDestination - 1] = this;
+    }//if
   }//move method
 
   @Override
