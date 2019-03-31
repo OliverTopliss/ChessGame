3eff1d76212f312273f0e37ad16f7e3a016b1860
Oliver Topliss <oliver.topliss1603@gmail.com>
2019-03-31 23:03:13 +0100

    added implementation for the move method and modified constructor method to take and accept parameters and arguments for the start x and y coordinates

--- com/olivertopliss/pieces/Rook.java
+++ com/olivertopliss/pieces/Rook.java
@@ -1,16 +1,31 @@
 package com.olivertopliss.pieces;
+
+import com.olivertopliss.game.Board;
+import static java.lang.Math.abs;
+
 public class Rook extends Piece
 {
 
-  public Rook(String team)
+  public Rook(String team, int startXCoordinate, int startYCoordinate)
   {
-    super(team);
+    super(team, startXCoordinate, startYCoordinate);
   }//Rook Constructor
 
   @Override
-  public void move()
+  public void move(int xDestination, int yDestination)
   {
-
+    //allows for vertical and horizontal motions only
+    if (abs(getCurrentXCoordinate() - xDestination) == 0 && abs(getCurrentYCoordinate() - yDestination) > 0
+       || (abs(getCurrentXCoordinate() - xDestination) > 0 && abs(getCurrentYCoordinate() - yDestination) == 0))
+    {
+      //clears the rook current position
+      Board.getBoard()[getCurrentXCoordinate() - 1][getCurrentXCoordinate() - 1] = null;
+      //updates the rook coordinates
+      setCurrentXCoordinate(xDestination);
+      setCurrentYCoordinate(yDestination);
+      //moves the rook to the new  location
+      Board.getBoard()[xDestination - 1][yDestination - 1] = this;
+    }//if
   }//move method
 
   @Override
