c73376e117cede297409c19c29d8b1be2240e2a9
Oliver Topliss <oliver.topliss1603@gmail.com>
2019-03-31 23:53:27 +0100

    added functionality to continually loop and accept input coordinates to move a piece

--- com/olivertopliss/game/Game.java
+++ com/olivertopliss/game/Game.java
@@ -1,4 +1,7 @@
 package com.olivertopliss.game;
+
+import java.util.Scanner;
+
 public class Game
 {
   private Board board;
@@ -13,6 +16,35 @@ public class Game
   {
     board.initialise();
   }// initialise method
+
+  //method used to simulate the game flow
+  public void start()
+  {
+    Scanner coordinateInput = new Scanner(System.in);
+    boolean gameOver = false;
+    String[] pieceToMoveCoordinates;
+    String[] pieceDestinationCoordinates;
+    int pieceToMoveXCoordinate = 0, pieceToMoveYCoordinate = 0, destinationXCoordinate = 0, destinationYCoordinate = 0;
+
+    //loops while there isn't a winner
+    while (!gameOver)
+    {
+      //gets the comma separated input coordinates and stores them in an array
+      pieceToMoveCoordinates = (coordinateInput.nextLine().split(","));
+      //assigns the x and y coordinates
+      pieceToMoveXCoordinate = Integer.parseInt(pieceToMoveCoordinates[0]);
+      pieceToMoveYCoordinate = Integer.parseInt(pieceToMoveCoordinates[1]);
+
+      //gets the comma separated input coordinates and stores them in an array
+      pieceDestinationCoordinates = (coordinateInput.nextLine().split(","));
+      //assigns the x and y coordinates
+      destinationXCoordinate = Integer.parseInt(pieceDestinationCoordinates[0]);
+      destinationYCoordinate = Integer.parseInt(pieceDestinationCoordinates[1]);
+      //moves a piece to the x and y coordinates
+      Board.getBoard()[pieceToMoveXCoordinate][pieceToMoveYCoordinate].move(destinationXCoordinate, destinationYCoordinate);
+      System.out.println();
+    }
+  }// start method
 
   // access for method for board
   public Board getBoard()
