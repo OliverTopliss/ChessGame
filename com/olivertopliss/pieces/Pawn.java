89a880a0d1ba42b646355c66b8f723b88fed2c5c
Oliver Topliss <oliver.topliss1603@gmail.com>
2019-03-31 00:09:31 +0000

    add implementation for the move method and also modified the constructor to accept and provide parameters and arguments for the current x and y coordinates

--- com/olivertopliss/pieces/Pawn.java
+++ com/olivertopliss/pieces/Pawn.java
@@ -1,14 +1,29 @@
 package com.olivertopliss.pieces;
+
+import com.olivertopliss.game.Board;
+import static java.lang.Math.abs;
+
 public class Pawn extends Piece
 {
 
-  public Pawn(String team)
+  public Pawn(String team, int startXCoordinate, int startYCoordinate)
   {
-    super(team);
+    super(team, startXCoordinate, startYCoordinate);
   }//Pawn Constructor
 
-  public void move()
+  public void move(int xDestination, int yDestination)
   {
+    // if the motion is only vertical by 1 space then it is valid
+    if((getCurrentXCoordinate() - xDestination == 0) && (abs(getCurrentYCoordinate() - yDestination) == 1))
+    {
+      //clears the pawn's current position
+      Board.getBoard()[getCurrentXCoordinate() - 1][getCurrentXCoordinate() - 1] = null;
+      //updates the bishops coordinates
+      setCurrentXCoordinate(xDestination);
+      setCurrentYCoordinate(yDestination);
+      //moves the pawn to the new  location
+      Board.getBoard()[xDestination - 1][yDestination - 1] = this;
+    }// if
 
   }//move method
 
