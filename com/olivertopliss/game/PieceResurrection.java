7c4111f30c9788e78907e91ad84d86233981b684
Oliver Topliss <oliver.topliss@student.manchester.ac.uk>
2019-06-20 19:48:48 +0100

    PieceRessurection now extends JFrame so a GUI can be constructed

--- com/olivertopliss/game/PieceResurrection.java
+++ com/olivertopliss/game/PieceResurrection.java
@@ -1,5 +1,7 @@
 //class that will handle the logic and GUI for choosing a piece to resurrect
-public class PieceResurrection
+
+import javax.swing.JFrame;
+public class PieceResurrection extends JFrame
 {
   //constructor method
   public PieceResurrection()
