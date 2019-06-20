c9c352e62bb8ddae4374d45a2b2b624b83722d7f
Oliver Topliss <oliver.topliss@student.manchester.ac.uk>
2019-06-20 19:44:26 +0100

    Created a class to model the logic and GUI for resurrecting a piece

---
+++ com/olivertopliss/game/PieceResurrection.java
@@ -0,0 +1,9 @@
+//class that will handle the logic and GUI for choosing a piece to resurrect
+public class PieceResurrection
+{
+  //constructor method
+  public PieceResurrection()
+  {
+
+  }// PieceResurrection method
+}//PieceRessurection Class
