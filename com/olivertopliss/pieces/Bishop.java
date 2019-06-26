691b46516b8938936a4c9e5b405911d46986cf21
Oliver Topliss <oliver.topliss@student.manchester.ac.uk>
2019-06-26 20:29:19 +0100

    Created a method to determine if the bishop's motion is blocked by a piece that isn't being taken

--- com/olivertopliss/pieces/Bishop.java
+++ com/olivertopliss/pieces/Bishop.java
@@ -13,8 +13,9 @@ public class Bishop extends Piece
   @Override
   public void move(int xDestination, int yDestination)
   {
+    boolean validMove = isValidMove(xDestination, yDestination);
     //checks if the requested move is legal
-    if((abs(getCurrentXCoordinate() - xDestination) == abs(getCurrentYCoordinate() - yDestination)))
+    if((abs(getCurrentXCoordinate() - xDestination) == abs(getCurrentYCoordinate() - yDestination) && validMove))
     {
       //clears the bishops current position
       Game.setBoard(getCurrentXCoordinate(),getCurrentYCoordinate(), null);
@@ -33,4 +34,69 @@ public class Bishop extends Piece
   {
     return "o";
   }//toString method
+
+  //method which determines whether a move can go ahead
+  //checks that there are no pieces in the way of the motion
+  public boolean isValidMove(int destinationXCoordinate, int destinationYCoordinate)
+  {
+    //checks diagonal aspect of the motion
+    //both x and y are increasing \.
+    if (destinationXCoordinate > getCurrentXCoordinate() && destinationYCoordinate > getCurrentYCoordinate())
+    {
+      //row and column are the current position + 1 so that the starting piece isn't compared to itself
+      int column = getCurrentYCoordinate() + 1;
+      for (int row = getCurrentXCoordinate() + 1; row < destinationXCoordinate; row++, column++)
+      {
+        if (!(Game.getBoard(row, column) == null))
+        {
+          return false;
+        }//if
+      }//for
+      return true;
+    }//if
+
+    //both x and y are decreasing ^\
+    else if(destinationXCoordinate < getCurrentXCoordinate() && destinationYCoordinate < getCurrentYCoordinate())
+    {
+      int column = getCurrentYCoordinate() - 1;
+      for (int row = getCurrentXCoordinate() - 1; row > destinationXCoordinate; row--, column--)
+      {
+        if (!(Game.getBoard(row, column) == null))
+        {
+          return false;
+        }//if
+      }//for
+      return true;
+    }//else if
+
+    //x increases and y decreases /^
+    else if(destinationXCoordinate > getCurrentXCoordinate() && destinationYCoordinate < getCurrentYCoordinate())
+    {
+      int column = getCurrentYCoordinate() - 1;
+      for (int row = getCurrentXCoordinate() + 1; row < destinationXCoordinate; row++, column--)
+      {
+        if (!(Game.getBoard(row, column) == null))
+        {
+          return false;
+        }//if
+      }//for
+      return true;
+    }//else if
+
+    //x decreases and y increases v/
+    else
+    {
+      int column = getCurrentYCoordinate() + 1;
+      for (int row = getCurrentXCoordinate() - 1; row > destinationXCoordinate; row--, column++)
+      {
+        if (!(Game.getBoard(row, column) == null))
+        {
+          return false;
+        }//if
+      }//for
+      return true;
+    }//else if
+
+
+  }//isValidMove method
 }//Bishop Class
